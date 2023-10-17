2d9b0979d704fe2cc736b6c64a73f8022a0b6e90
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-17 18:38:42 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        void MergeSortedArrays(int[] firstArr, int[] SndArr) {
+
+            int TotalSize = firstArr.Length + SndArr.Length;
+            int[] arrIntem = new int[TotalSize];
+            int contador = 0;
+
+            for (int i = 0; i < firstArr.Length; i++) {
+                arrIntem[i] = firstArr[i];
+                contador++;
+            }
+
+            for (int i = contador; i < TotalSize; i++) {
+                arrIntem[i] = SndArr[SndArr.Length - i];
+            }
+
+            foreach (int i in arrIntem) {
+                Console.WriteLine(i);
+            }
+
+            
+
+        }
+        int[] arr1 = { 1, 2, 3 };
+        int[] arr2 = { 4, 5, 3 };
+        MergeSortedArrays(arr1,arr2);    
+    }
+}
